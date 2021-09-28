03206f4cc256cb244a26e9c9d564846f9b9367e4
LeonardoTozzi <leonardotozzis@gmail.com>
2021-09-27 22:32:40 -0300

    EstudoReact

---
+++ src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+function Home(){
+    return(
+        <View>
+            <Text>TELA HOME</Text>
+        </View>
+    )
+}
+export default Home; //exportação para outros arquivos terem acesso.
---
+++ src/pages/Movies/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+function Movies(){
+    return(
+        <View>
+            <Text>TELA MEUS FILMES</Text>
+        </View>
+    )
+}
+export default Movies; //exportação para outros arquivos terem acesso.
